12243a62151a6167d58c391abd5d782c8469cf3b
Temur5319436 <temur5319436@gmail.com>
2023-12-31 12:01:13 +0500

    refactor: shoot_down function is removed

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ async def on_startup(dp):
     await bot.set_webhook(f"{config.WEBHOOK_HOST}{config.WEBHOOK_PATH}")
 
 
-async def on_shutdown(dp):
-    await bot.delete_webhook()
-
-
 def main():
     if config.ENV == "production":
         executor.start_webhook(
@@ -20,7 +16,6 @@ def main():
             webhook_path=config.WEBHOOK_PATH,
             skip_updates=True,
             on_startup=on_startup,
-            on_shutdown=on_shutdown,
             host=config.WEBAPP_HOST,
             port=config.WEBAPP_PORT,
         )
